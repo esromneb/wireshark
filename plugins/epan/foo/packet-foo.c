3f4cd0fde46af01c1789d2073ac6e38b8ca50d9d
Ben Morse <bjmorse@gmail.com>
2022-07-12 00:13:24 -0700

    following https://gitlab.com/wireshark/wireshark/-/blob/master/docbook/wsdg_src/WSDG_chapter_dissection.adoc
    can mark the first byte as a field

--- plugins/epan/foo/packet-foo.c
+++ plugins/epan/foo/packet-foo.c
@@ -6,22 +6,54 @@
 
 static int proto_foo = -1;
 
+static int hf_foo_pdu_type = -1;
+static gint ett_foo = -1;
+
+
 void
 proto_register_foo(void)
 {
+    static hf_register_info hf[] = {
+        { &hf_foo_pdu_type,
+            { "FOO PDU Type", "foo.type",
+            FT_UINT8, BASE_DEC,
+            NULL, 0x0,
+            NULL, HFILL }
+        }
+    };
+
+    /* Setup protocol subtree array */
+    static gint *ett[] = {
+        &ett_foo
+    };
+
+
+
     proto_foo = proto_register_protocol (
         "FOO Protocol", /* name       */
         "FOO",      /* short name */
         "foo"       /* abbrev     */
         );
+
+    proto_register_field_array(proto_foo, hf, array_length(hf));
+    proto_register_subtree_array(ett, array_length(ett));
+
 }
 
 static int
-dissect_foo(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree _U_, void *data _U_)
+dissect_foo(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data)
 {
+    (void)tree;
+    (void)data;
     col_set_str(pinfo->cinfo, COL_PROTOCOL, "FOO");
-    /* Clear out stuff in the info column */
-    col_clear(pinfo->cinfo,COL_INFO); 
+    
+    // Clear out stuff in the info column
+    col_clear(pinfo->cinfo, COL_INFO); 
+
+    proto_item *ti = proto_tree_add_item(tree, proto_foo, tvb, 0, -1, ENC_NA);
+    proto_tree *foo_tree = proto_item_add_subtree(ti, ett_foo);
+    proto_tree_add_item(foo_tree, hf_foo_pdu_type, tvb, 0, 1, ENC_BIG_ENDIAN);
+
 
     return tvb_captured_length(tvb);
 }
@@ -34,7 +66,4 @@ proto_reg_handoff_foo(void)
     foo_handle = create_dissector_handle(dissect_foo, proto_foo);
     dissector_add_uint("udp.port", FOO_PORT, foo_handle);
 }
-
-
-
 
