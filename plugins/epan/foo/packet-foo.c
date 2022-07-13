79292ca6c3b4e363b0095e5f1bc16c48fc8ce63b
Ben Morse <bjmorse@gmail.com>
2022-07-12 21:17:17 -0700

    more fields

--- plugins/epan/foo/packet-foo.c
+++ plugins/epan/foo/packet-foo.c
@@ -2,6 +2,8 @@
 
 #include <epan/packet.h>
 
+#include <stdio.h>
+
 #define FOO_PORT 1234
 
 static int proto_foo = -1;
@@ -9,14 +11,39 @@ static int proto_foo = -1;
 static int hf_foo_pdu_type = -1;
 static gint ett_foo = -1;
 
+static int hf_foo_flags = -1;
+static int hf_foo_sequenceno = -1;
+static int hf_foo_initialip = -1;
+
 
 void
 proto_register_foo(void)
 {
+    // this array gets passed to proto_register_field_array()
+    // it will set the static globals above with unique field id (I'm guessing)
+    // then later we pass these unique ids to fns in dissect_foo()
     static hf_register_info hf[] = {
         { &hf_foo_pdu_type,
             { "FOO PDU Type", "foo.type",
             FT_UINT8, BASE_DEC,
+            NULL, 0x0,
+            NULL, HFILL }
+        },
+        { &hf_foo_flags,
+            { "FOO PDU Flags", "foo.flags",
+            FT_UINT8, BASE_HEX,
+            NULL, 0x0,
+            NULL, HFILL }
+        },
+        { &hf_foo_sequenceno,
+            { "FOO PDU Sequence Number", "foo.seqn",
+            FT_UINT16, BASE_DEC,
+            NULL, 0x0,
+            NULL, HFILL }
+        },
+        { &hf_foo_initialip,
+            { "FOO PDU Initial IP", "foo.initialip",
+            FT_IPv4, BASE_NONE,
             NULL, 0x0,
             NULL, HFILL }
         }
@@ -38,6 +65,10 @@ proto_register_foo(void)
     proto_register_field_array(proto_foo, hf, array_length(hf));
     proto_register_subtree_array(ett, array_length(ett));
 
+    // printf("End of proto_register_foo()\n");
+    // printf("hf_foo_pdu_type : %d\n", hf_foo_pdu_type);
+    // printf("hf_foo_sequenceno : %d\n", hf_foo_sequenceno);
+    // printf("hf_foo_initialip : %d\n", hf_foo_initialip);
 }
 
 static int
@@ -50,9 +81,18 @@ dissect_foo(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data)
     // Clear out stuff in the info column
     col_clear(pinfo->cinfo, COL_INFO); 
 
+    gint offset = 0;
+
     proto_item *ti = proto_tree_add_item(tree, proto_foo, tvb, 0, -1, ENC_NA);
     proto_tree *foo_tree = proto_item_add_subtree(ti, ett_foo);
-    proto_tree_add_item(foo_tree, hf_foo_pdu_type, tvb, 0, 1, ENC_BIG_ENDIAN);
+    proto_tree_add_item(foo_tree, hf_foo_pdu_type, tvb, offset, 1, ENC_BIG_ENDIAN);
+    offset += 1;
+    proto_tree_add_item(foo_tree, hf_foo_flags, tvb, offset, 1, ENC_BIG_ENDIAN);
+    offset += 1;
+    proto_tree_add_item(foo_tree, hf_foo_sequenceno, tvb, offset, 2, ENC_BIG_ENDIAN);
+    offset += 2;
+    proto_tree_add_item(foo_tree, hf_foo_initialip, tvb, offset, 4, ENC_BIG_ENDIAN);
+    offset += 4;
 
 
     return tvb_captured_length(tvb);
