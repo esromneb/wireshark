09c3475b11177a74a67c45520b1e44e5a71ea132
Benjamin Morse <bjmorse@gmail.com>
2022-07-22 12:56:37 -0700

    copy unthread of register_all_proto

--- epan/register.c
+++ epan/register.c
@@ -13,6 +13,7 @@
 
 #include <glib.h>
 #include "epan/dissectors/dissectors.h"
+#include <stdio.h>
 
 static const char *cur_cb_name = NULL;
 // We could use g_atomic_pointer_set/get instead of a mutex, but that's
@@ -23,26 +24,55 @@ static GAsyncQueue *register_cb_done_q;
 #define CB_WAIT_TIME (150 * 1000) // microseconds
 
 static void set_cb_name(const char *proto) {
-    g_mutex_lock(&cur_cb_name_mtx);
+    // g_mutex_lock(&cur_cb_name_mtx);
     cur_cb_name = proto;
-    g_mutex_unlock(&cur_cb_name_mtx);
+    // g_mutex_unlock(&cur_cb_name_mtx);
 }
 
 static void *
 register_all_protocols_worker(void *arg _U_)
 {
     for (gulong i = 0; i < dissector_reg_proto_count; i++) {
+        printf("register_all_protocols_worker() %ld\n", i);
         set_cb_name(dissector_reg_proto[i].cb_name);
         dissector_reg_proto[i].cb_func();
     }
 
-    g_async_queue_push(register_cb_done_q, GINT_TO_POINTER(TRUE));
+    // g_async_queue_push(register_cb_done_q, GINT_TO_POINTER(TRUE));
     return NULL;
 }
 
 void
 register_all_protocols(register_cb cb, gpointer cb_data)
 {
+    printf("register_all_protocols()\n");
+    const char *cb_name;
+    register_cb_done_q = g_async_queue_new();
+    gboolean called_back = FALSE;
+    // GThread *rapw_thread;
+
+    register_all_protocols_worker(NULL);
+
+    // rapw_thread = g_thread_new("register_all_protocols_worker", &register_all_protocols_worker, NULL);
+    // while (!g_async_queue_timeout_pop(register_cb_done_q, CB_WAIT_TIME)) {
+        g_mutex_lock(&cur_cb_name_mtx);
+        cb_name = cur_cb_name;
+        // g_mutex_unlock(&cur_cb_name_mtx);
+        if (cb && cb_name) {
+            cb(RA_REGISTER, cb_name, cb_data);
+            called_back = TRUE;
+        }
+    // }
+    // g_thread_join(rapw_thread);
+    if (cb && !called_back) {
+        cb(RA_REGISTER, "finished", cb_data);
+    }
+}
+
+void
+register_all_protocols2(register_cb cb, gpointer cb_data)
+{
+    printf("register_all_protocols()\n");
     const char *cb_name;
     register_cb_done_q = g_async_queue_new();
     gboolean called_back = FALSE;
