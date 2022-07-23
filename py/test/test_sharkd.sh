fb63178ab63406290e4d0f46cb1cd04da16ad149
Benjamin Morse <bjmorse@gmail.com>
2022-07-22 17:00:56 -0700

    add diff this diff is from the next commit

--- py/test/test_sharkd.sh
+++ py/test/test_sharkd.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-../../build/install/bin/sharkd - < ./sharkd_in1.txt
+../../build/install/bin/sharkd - < ./sharkd_in1.txt | tee test1_output.txt
+
+echo Diff:
+
+diff test1_output_ideal.txt test1_output.txt
 
